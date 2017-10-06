60139431207689683d1b327d6c1a9e483b5e7dde
ChrisReid88 <40202859@live.napier.ac.uk>
2017-10-06 08:52:00 +0100

    Axis shown on board - needs changed to letters.

--- draughts.py
+++ draughts.py
@@ -7,18 +7,19 @@ board = [['-','w','-','w','-','w','-','w'],
          ['-','B','-','B','-','B','-','B'],
          ['B','-','B','-','B','-','B','-']]
 
-top = '1   2   3   4   5   6   7   8'
+top = '  1   2   3   4   5   6   7   8'
 
 def updateState():
-    print(top)             
+    print(top)   
+    i = 0
     for row in board:
-        print(' | '.join(map(str,row)))
-        print('-------------------------------')
-
+        i = i + 1
+        print(i,' | '.join(map(str,row)))
+        print('  -----------------------------')
 updateState()
 
 def whiteMove():
-    peiceToMove = input('Enter a peice to move(E.g 1,1): ')
+    peiceToMove = input('Enter a peice to move(E.g 1 1): ')
     x = int(peiceToMove[0])
     y = int(peiceToMove[2])
 
@@ -29,7 +30,6 @@ def whiteMove():
         y2 = int(placeToMove[2])
         if board[x][y] == '-':
             board[x2][y2] = 'w'
-            
     else:
         print("Sorry. Invalid selection")
     updateState()
