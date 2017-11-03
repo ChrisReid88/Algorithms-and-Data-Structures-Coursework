8f4bd8e06b727817812e1504ac57465a5121810c
ChrisReid88 <40202859@live.napier.ac.uk>
2017-11-03 13:20:40 +0000

    Changed to abide by PEP 8 layout and naming conventions

--- draughts.py
+++ draughts.py
@@ -9,29 +9,34 @@ board = [['-','w','-','w','-','w','-','w'],
 
 top = '  1   2   3   4   5   6   7   8'
 
-def updateState():
+
+def update_state():
     print(top)   
     i = 0
     for row in board:
         i = i + 1
-        print(i,' | '.join(map(str,row)))
-        print('  -----------------------------')
-updateState()
+        print i,' | '.join(map(str, row))
+        print '  -----------------------------'
 
-def whiteMove():
-    peiceToMove = input('Enter a peice to move(E.g 1 1): ')
-    x = int(peiceToMove[0])
-    y = int(peiceToMove[2])
+
+update_state()
+
+
+def white_move():
+    peice_to_move = raw_input('Enter a peice to move(E.g 1 1): ')
+    x = int(peice_to_move[0])
+    y = int(peice_to_move[2])
 
     if board[x][y] == 'w':
         board[x][y] = '-'
-        placeToMove = input('Select where you want to move: ')
-        x2 = int(placeToMove[0])
-        y2 = int(placeToMove[2])
+        place_to_move = raw_input('Select where you want to move: ')
+        x2 = int(place_to_move[0])
+        y2 = int(place_to_move[2])
         if board[x][y] == '-':
             board[x2][y2] = 'w'
     else:
-        print("Sorry. Invalid selection")
-    updateState()
-    
-whiteMove()
+        print "Sorry. Invalid selection"
+    update_state()
+
+
+white_move()
